1ad5cc32eacbe73916025c0e90caff7c7e1bf206
Alexey <iamfullofspam@gmail.com>
2014-06-23 16:20:18 +0400

    comment updated in config.py

--- IPythonWorkflow/config.py
+++ IPythonWorkflow/config.py
@@ -1,6 +1,9 @@
 # Global constants,
 # if you need to override them, do it in
 # configlocal.py (create the file if it doesn't exist)
+
+# If there is some machine-specific initialization code you want to run,
+# you can place it in configlocal.py too
 ipc_profile = None
 
 try:
