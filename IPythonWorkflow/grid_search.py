c504d406c5a887076e7927a18f98c343fa1d2d5d
Alexey <iamfullofspam@gmail.com>
2014-08-26 13:56:32 +0400

    minor fix

--- IPythonWorkflow/grid_search.py
+++ IPythonWorkflow/grid_search.py
@@ -436,7 +436,7 @@ class GridOptimalSearchCV(BaseEstimator, ClassifierMixin):
                 assert len(result) == portion, "The length of result is very strange"
                 for state_indices, state_dict, score in zip(state_indices_array, state_dict_array, result):
                     params = ", ".join([k + '=' + str(v) for k, v in state_dict.iteritems()])
-                    if isinstance(Exception, score):
+                    if isinstance(score, Exception):
                         # returned exception
                         message = 'Fail during training on the node \nException ' + str(score) +\
                                   '\nParameters:' + params
