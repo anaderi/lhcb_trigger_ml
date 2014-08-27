5e05db564923274145e2143e3390bef6d6ac493f
Alexey <iamfullofspam@gmail.com>
2014-08-27 21:30:40 +0400

    Binner moved to commonutils

--- IPythonWorkflow/binninguniforming.py
+++ IPythonWorkflow/binninguniforming.py
@@ -1,5 +1,7 @@
 import numpy
-import commonutils
+from commonutils import check_sample_weight, weighted_percentile, \
+    build_normalizer, generate_sample
+from numpy.random.mtrand import RandomState
 from sklearn.base import BaseEstimator, ClassifierMixin, clone
 from sklearn.cross_validation import train_test_split
 
@@ -16,6 +18,76 @@ __author__ = 'Alex Rogozhnikov'
 # this module is concerned deprecated
 
 # TODO try doing the same without binning
+
+
+class Binner:
+    def __init__(self, values, n_bins, sample_weight=None):
+        """Binner is a class that helps to split the values into several bins.
+        Initially an array of values is given, which is then splitted into 'bins_number' equal parts,
+        and thus we are computing limits (boundaries of bins)."""
+        percentiles = [i / n_bins for i in range(1, n_bins)]
+        sample_weight = check_sample_weight(values, sample_weight=sample_weight)
+        self.limits = weighted_percentile(values, percentiles, sample_weight=sample_weight)
+
+    def get_bins(self, values):
+        return numpy.searchsorted(self.limits, values)
+
+    def set_limits(self, limits):
+        self.limits = limits
+
+    @property
+    def n_bins(self):
+        return len(self.limits) + 1
+
+    def split_into_bins(self, *arrays):
+        """Splits the data of parallel arrays into bins, the first array is binning variable"""
+        values = arrays[0]
+        numpy_arrays = []
+        for i, array in enumerate(arrays):
+            assert len(array) == len(values), "passed arrays have different lengths"
+            numpy_arrays.append(numpy.array(array))
+        bins = self.get_bins(values)
+        result = []
+        for bin in range(len(self.limits) + 1):
+            indices = bins == bin
+            result.append([array[indices] for array in numpy_arrays])
+        return result
+
+
+def test_binner():
+    """This function tests binner class"""
+    random = RandomState()
+    binner = Binner(random.permutation(30), 3)
+    assert numpy.all(binner.limits > [9, 19]), 'failed on the limits'
+    assert numpy.all(binner.limits < [10, 20]), 'failed on the limits'
+    bins = binner.get_bins([-1000, 1000, 0, 10, 20, 9.0, 10.1, 19.0, 20.1])
+    assert numpy.all(bins == [0, 2, 0, 1, 2, 0, 1, 1, 2]), 'wrong binning'
+
+    binner = Binner(random.permutation(20), 5)
+    p = random.permutation(40)
+    # checking whether binner preserves correspondence
+    list1 = list(binner.split_into_bins(numpy.array(range(-10, 30))[p], numpy.array(range(0, 40))[p]))
+    for a, b in list1:
+        for x, y in zip(a, b):
+            assert x + 10 == y, 'transpositions are wrong after binning'
+
+    binner = Binner(random.permutation(30), 3)
+    result2 = list(binner.split_into_bins(range(10, 20)))
+    answer2 = [[], range(10, 20), []]
+
+    for a, b in zip(result2, answer2):
+        for x, y in zip(a[0], b):
+            assert x == y, 'binning is wrong'
+
+    result3 = list(binner.split_into_bins(random.permutation(45)))
+    answer3 = list(binner.split_into_bins(range(45)))
+    for x, y in zip(result3, answer3):
+        assert set(x[0]) == set(y[0]), "binner doesn't work well with permutations"
+
+    print('binner is ok')
+
+test_binner()
+
 
 
 def get_train_test_data_for_bin(bins_data, bin_index, mode='outer'):
@@ -69,7 +141,7 @@ class BinningUniformClassifier(BaseEstimator, ClassifierMixin):
 
         masses = X[self.uniform_variables[0]]
         # binning
-        self.binner = commonutils.Binner(masses, self.n_bins)
+        self.binner = Binner(masses, self.n_bins)
         self.classifiers = []
         self.normalizers = []
         bins_data = list(self.binner.split_into_bins(masses, self.get_train_variables(X), y))
@@ -81,7 +153,7 @@ class BinningUniformClassifier(BaseEstimator, ClassifierMixin):
             bin_classifier = clone(self.base_estimator)
             self.classifiers.append(bin_classifier.fit(binFitX, binFitY))
             predictions = bin_classifier.predict_proba(binCorrX)
-            self.normalizers.append(commonutils.build_normalizer(predictions[binCorrY > 0.5, 1]))
+            self.normalizers.append(build_normalizer(predictions[binCorrY > 0.5, 1]))
 
         return self
 
@@ -116,8 +188,8 @@ def test_binning_uniforming():
     from sklearn.naive_bayes import GaussianNB
     from sklearn.metrics import roc_auc_score
 
-    trainX, trainY = commonutils.generate_sample(1000, 10, 2)
-    testX, testY = commonutils.generate_sample(1000, 10, 2)
+    trainX, trainY = generate_sample(1000, 10, 2)
+    testX, testY = generate_sample(1000, 10, 2)
 
     uniform_variables = [trainX.columns[0]]
 
