055e7979f86952ca9bec2ed4005193ad6b1b748b
Alexey <iamfullofspam@gmail.com>
2015-02-17 21:32:38 +0300

    updated tests for gb and new trees

--- tests/test_fastgb.py
+++ tests/test_fastgb.py
@@ -3,29 +3,104 @@ from __future__ import division, print_function, absolute_import
 import numpy
 from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
 from sklearn.metrics.metrics import roc_auc_score
+from sklearn.tree.tree import DecisionTreeRegressor
 from hep_ml.commonutils import generate_sample
-from hep_ml.ugradientboosting import AdaLossFunction
-from hep_ml.experiments.fastgb import FastGradientBoostingClassifier
+from hep_ml.ugradientboosting import AdaLossFunction, BinomialDevianceLossFunction as BinomialDeviance, \
+    uGradientBoostingClassifier
+from hep_ml.experiments.categorical import CategoricalTreeRegressor, SimpleCategorizer, ComplexCategorizer
+from hep_ml.experiments.fasttree import FastTreeRegressor, FastNeuroTreeRegressor
+from hep_ml.experiments.fastgb import TreeGradientBoostingClassifier, CommonGradientBoosting, FoldingGBClassifier
+import time
+
 __author__ = 'Alex Rogozhnikov'
 
 
-def test_gb_quality(n_samples=1000):
-    X, y = generate_sample(n_samples=n_samples, n_features=5)
-    X = numpy.array(X)
+def test_workability(n_samples=10000, n_features=10, distance=0.5):
+    trainX, trainY = generate_sample(n_samples=n_samples, n_features=n_features, distance=distance)
+    testX, testY = generate_sample(n_samples=n_samples, n_features=n_features, distance=distance)
+    for booster in [FoldingGBClassifier, TreeGradientBoostingClassifier]:
+        for loss in [BinomialDeviance(), AdaLossFunction()]:
+            for update in [True, False]:
+                for base in [FastTreeRegressor(max_depth=3), FastNeuroTreeRegressor(max_depth=3)]:
+                    clf = booster(loss=loss, n_estimators=100,
+                                  base_estimator=base, update_tree=update)
+                    clf.fit(trainX, trainY)
+                    print('booster', booster, loss, 'update=', update, ' base=', base.__class__,
+                          ' quality=', roc_auc_score(testY, clf.predict_proba(testX)[:, 1]))
 
-    gb = FastGradientBoostingClassifier(loss=AdaLossFunction(), n_estimators=100, min_samples_split=50, max_depth=5)
-    gb = gb.fit(X, y)
 
-    gb_old = GradientBoostingClassifier(n_estimators=100, min_samples_split=50, max_depth=5)
-    gb_old = gb_old.fit(X, y)
+# test_workability()
 
-    testX, testy = generate_sample(n_samples=n_samples + 1, n_features=5)
-    testX = numpy.array(testX)
 
-    auc = roc_auc_score(testy, gb.predict(testX))
-    auc_old = roc_auc_score(testy, gb_old.predict(testX))
-    print("AUC new", auc)
-    print("AUC old", auc_old)
-    assert auc > 0.95, auc
+def test_gb_quality(n_samples=10000, n_features=10, distance=0.5):
+    trainX, trainY = generate_sample(n_samples=n_samples, n_features=n_features, distance=distance)
+    testX, testY = generate_sample(n_samples=n_samples, n_features=n_features, distance=distance)
 
-test_gb_quality()
+    # Multiplying by random matrix
+    multiplier = numpy.random.normal(size=[n_features, n_features])
+    shift = numpy.random.normal(size=[1, n_features]) * 5
+    trainX = numpy.dot(trainX.values, multiplier) + shift
+    testX = numpy.dot(testX.values, multiplier) + shift
+
+    boosters = {
+        'old_boost': GradientBoostingClassifier(n_estimators=100, min_samples_split=50, max_depth=5, subsample=0.3),
+        'fast+old_tree': CommonGradientBoosting(n_estimators=100,
+            base_estimator=DecisionTreeRegressor(min_samples_split=50, max_depth=5)),
+        'fast+neuro': TreeGradientBoostingClassifier(n_estimators=100, update_tree=True,
+                                                     base_estimator=FastNeuroTreeRegressor()),
+        'fold+tree': FoldingGBClassifier(loss=BinomialDeviance(), n_estimators=10, update_tree=True,
+                                         base_estimator=FastNeuroTreeRegressor()),
+        'ugb': uGradientBoostingClassifier(loss=AdaLossFunction(),
+            n_estimators=100, min_samples_split=50, max_depth=5, update_tree=True, subsample=0.3)
+    }
+
+    for criterion in ['mse', # 'fmse', # 'pvalue',
+                      # 'significance',
+                      'significance2',
+                      # 'gini',
+                      'entropy',
+                      'poisson'
+    ]:
+        boosters['fast-' + criterion[:4]] = TreeGradientBoostingClassifier(n_estimators=100, update_tree=True,
+            base_estimator=FastTreeRegressor(criterion=criterion))
+
+    for name, booster in boosters.items():
+        start = time.time()
+        booster.fit(trainX, trainY)
+        auc = roc_auc_score(testY, booster.predict_proba(testX)[:, 1])
+        print(name, "spent:{:3.2f} auc:{}".format(time.time() - start, auc))
+
+        # assert new_auc > 0.7, new_auc
+
+
+test_gb_quality(n_samples=100000)
+
+
+def test_categorical_gb(n_samples=100000, n_features=10, p=0.7):
+    y = numpy.random.random(n_samples) > 0.5
+    X = numpy.random.randint(40, size=[n_samples, n_features]) * 2
+    X += numpy.random.random(size=[n_samples, n_features]) > p
+    X += y[:, numpy.newaxis]
+
+    from sklearn.cross_validation import train_test_split
+
+    trainX, testX, trainY, testY = train_test_split(X, y)
+    boosters = {
+        'old': GradientBoostingClassifier(n_estimators=100, min_samples_split=50, max_depth=5),
+        'cat': CommonGradientBoosting(loss=AdaLossFunction(), subsample=0.5, dtype=int,
+            base_estimator=CategoricalTreeRegressor()),
+        'cat2': TreeGradientBoostingClassifier(loss=BinomialDeviance(), dtype='int', update_tree=False,
+            base_estimator=SimpleCategorizer(n_features=2, n_attempts=3, method='cv')),
+        'cat3': TreeGradientBoostingClassifier(loss=BinomialDeviance(), dtype='int', update_tree=False,
+            base_estimator=ComplexCategorizer(n_features=10, n_categories_power=5, splits=1, pfactor=0.5)),
+        'cat2-2': TreeGradientBoostingClassifier(loss=BinomialDeviance(), dtype='int', update_tree=False, n_threads=2,
+            base_estimator=SimpleCategorizer(n_features=2, n_attempts=1)),
+    }
+    for name, booster in boosters.items():
+        start = time.time()
+        booster.fit(trainX, trainY)
+        auc = roc_auc_score(testY, booster.predict_proba(testX)[:, 1])
+        print(name, "spent:{:3.2f} auc:{}".format(time.time() - start, auc))
+
+
+# test_categorical_gb()
--- tests/test_fasttree.py
+++ tests/test_fasttree.py
@@ -1,12 +1,13 @@
 from __future__ import division, print_function, absolute_import
+from collections import OrderedDict
 
 import numpy
+import time
 from sklearn.metrics import roc_auc_score
 from hep_ml.commonutils import generate_sample
 from hep_ml.experiments.fasttree import FastTreeRegressor
 from sklearn.tree import DecisionTreeRegressor
-# for super-puper hack
-from six.moves import builtins
+
 __author__ = 'Alex Rogozhnikov'
 
 
@@ -18,26 +19,59 @@ def test_tree(n_samples=1000):
     tree = tree.fit(X, y, sample_weight=w)
     prediction = tree.predict(X)
     tree.print_tree_stats()
-    print(prediction)
     auc = roc_auc_score(y, prediction)
     print("AUC", auc)
     assert auc > 0.7, auc
 
 
 def test_tree_speed(n_samples=100000, n_features=10):
-    import timeit
-    tree_regressor = DecisionTreeRegressor(max_depth=10, min_samples_split=50)
-    tree_fast_regressor = FastTreeRegressor(max_depth=10, min_samples_split=50)
     X, y = generate_sample(n_samples=n_samples, n_features=n_features)
     X = numpy.array(X)
     w = numpy.ones(n_samples)
-    timer1 = timeit.Timer('tree_regressor.fit(X, y, sample_weight=w)')
-    timer2 = timeit.Timer('tree_fast_regressor.fit(X, y, sample_weight=w)')
-    # super-puper-hack!
-    builtins.__dict__.update(locals())
 
-    time1 = timer1.timeit(1)
-    time2 = timer2.timeit(1)
-    print('times', time1, time2)
-    assert time1 > time2
+    regressors = OrderedDict()
+    regressors['old'] = DecisionTreeRegressor(max_depth=10, min_samples_split=50)
+    regressors['new'] = FastTreeRegressor(max_depth=10, min_samples_split=50)
 
+    for name, regressor in regressors.items():
+        start = time.time()
+        for _ in range(3):
+            regressor.fit(X, y, sample_weight=w)
+        print(name, 'trains in ', time.time() - start)
+
+    # Testing speed of prediction:
+    methods = OrderedDict()
+    methods['old'] = lambda: regressors['old'].predict(X)
+    methods['new'] = lambda: regressors['new'].apply(X)
+    methods['new-fast'] = lambda: regressors['new'].fast_apply(X)
+    for name, method in methods.items():
+        start = time.time()
+        for _ in range(5):
+            method()
+        print(name, 'requires ', time.time() - start)
+
+
+def tree_quality_comparison(n_samples=200000, n_features=10):
+    trainX, trainY = generate_sample(n_samples=n_samples, n_features=n_features)
+    testX, testY = generate_sample(n_samples=n_samples, n_features=n_features)
+
+    # Multiplying by random matrix
+    multiplier = numpy.random.normal(size=[n_features, n_features])
+    trainX = numpy.dot(trainX.values, multiplier)
+    testX = numpy.dot(testX.values, multiplier)
+    regressors = OrderedDict()
+    regressors['old'] = DecisionTreeRegressor(max_depth=10, min_samples_split=50)
+    regressors['new'] = FastTreeRegressor(max_depth=10, min_samples_split=50, criterion='order')
+    w = numpy.ones(n_samples)
+
+    for name, regressor in regressors.items():
+        regressor.fit(trainX, trainY, sample_weight=w)
+        print(name, roc_auc_score(testY, regressor.predict(testX)))
+
+    # Testing apply method
+    indices1, values1 = regressors['new'].apply(testX)
+    indices2, values2 = regressors['new'].fast_apply(testX)
+    assert numpy.all(values1 == values2), 'two apply methods give different results'
+
+
+tree_quality_comparison(n_samples=1000)
